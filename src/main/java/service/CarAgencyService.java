7e0969686875dfa0528ac32481421d379a57af1d
mohammed <mohammedre4a@gmail.com>
2022-11-23 22:28:32 +0200

    feat: add update and delete functions to CarAgencyService. refactor: fix session issue in CarAgencyService

--- src/main/java/service/CarAgencyService.java
+++ src/main/java/service/CarAgencyService.java
@@ -4,40 +4,77 @@ import model.Car;
 import model.CarAgency;
 import model.CarAgency;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class CarAgencyService {
-    Session session;
-    Transaction transaction;
+    SessionFactory factory;
 
-    public CarAgencyService(Session session) {
-        this.session = session;
-        transaction = session.beginTransaction();
+    public CarAgencyService(SessionFactory factory) {
+        this.factory = factory;
     }
 
     public CarAgency saveCarAgency(CarAgency carAgency) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
         session.persist(carAgency);
         transaction.commit();
+        session.close();
         return carAgency;
     }
 
+    public CarAgency updateCarAgency(CarAgency carAgency) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("UPDATE CarAgency ca SET ca.agencyName =: agencyName, ca.cars =: cars WHERE ca.id =: id", CarAgency.class);
+        query.setParameter("agencyName", carAgency.getAgencyName());
+        query.setParameter("cars", carAgency.getCars());
+        query.setParameter(3, carAgency.getId());
+        transaction.commit();
+        session.close();
+        if (query.executeUpdate() > 0)
+            return carAgency;
+        return null; //can't be updated
+    }
+
+    public CarAgency deleteCar(CarAgency carAgency) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("DELETE from CarAgency ca WHERE ca.id =: id");
+        query.setParameter(1, carAgency.getId());
+        transaction.commit();
+        session.close();
+        if (query.executeUpdate() > 0)
+            return carAgency;
+        return null; //can't be deleted
+    }
+
     public List<CarAgency> selectAll() {
-        return session.createQuery("select DISTINCT ca from CarAgency ca", CarAgency.class).getResultList();
+        Session session = factory.openSession();
+        List<CarAgency> all = session.createQuery("select DISTINCT ca from CarAgency ca", CarAgency.class).getResultList();
+        session.close();
+        return all;
     }
 
     public CarAgency getCarAgencyById(Long id) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT ca from CarAgency ca where ca.id=: id", CarAgency.class);
         query.setParameter(1, id);
-        return (CarAgency) query.getSingleResultOrNull();
+        CarAgency agency = (CarAgency) query.getSingleResultOrNull();
+        session.close();
+        return agency;
     }
 
     public List<CarAgency> getCarAgencyByName(String name) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT ca from CarAgency ca where ca.agencyName=: type", CarAgency.class);
         query.setParameter(1, name);
-        return query.getResultList();
+        List<CarAgency> agencies = query.getResultList();
+        session.close();
+        return agencies;
     }
 
     public List<Car> getAgencyCars(Long id) {
