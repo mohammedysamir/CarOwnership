495790cfb171c2abe40db5a0b0e5dc2d55000b33
mohammed <mohammedre4a@gmail.com>
2022-11-23 21:30:01 +0200

    fix: fix transaction issue.

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -3,72 +3,94 @@ package service;
 import model.Car;
 import model.Engine;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class CarService {
-    public CarService(Session session) {
-        this.session = session;
-        transaction = session.beginTransaction();
+    public CarService(SessionFactory factory) {
+        this.factory = factory;
     }
 
-    Session session;
-    Transaction transaction;
+    SessionFactory factory;
 
     public Car saveCar(Car car) {
-        this.session.persist(car);
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(car);
         transaction.commit();
         return car;
     }
 
     public Car updateCar(Car car) {
-        Query query = session.createQuery("UPDATE Car c SET c.color =: color, c.model =: model, c.engine=:engine WHERE c.id =: id",Car.class);
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        Query query = session.createQuery("UPDATE Car c SET c.color =: color, c.model =: model, c.engine=:engine WHERE c.id =: id", Car.class);
         query.setParameter(1, car.getColor());
         query.setParameter(2, car.getModel());
         query.setParameter(3, car.getEngine());
         query.setParameter(4, car.getId());
         transaction.commit();
+        session.close();
         if (query.executeUpdate() > 0)
             return car;
         return null; //can't be updated
     }
 
     public Car deleteCar(Car car) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
         Query query = session.createQuery("DELETE from Car c WHERE c.id =: id");
         query.setParameter(1, car.getId());
         transaction.commit();
+        session.close();
         if (query.executeUpdate() > 0)
             return car;
         return null; //can't be deleted
     }
 
     public List<Car> selectAll() {
-        return session.createQuery("select DISTINCT car from Car car", Car.class).getResultList();
+        Session session = factory.openSession();
+        List<Car> all = session.createQuery("select DISTINCT car from Car car", Car.class).getResultList();
+        session.close();
+        return all;
     }
 
     public Car getCarById(Long id) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT car from Car car where car.id=: id", Car.class);
         query.setParameter(1, id);
-        return (Car) query.getSingleResultOrNull();
+        Car result = (Car) query.getSingleResultOrNull();
+        session.close();
+        return result;
     }
 
     public List<Car> getCarByModel(String model) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT car from Car car where car.model=: model", Car.class);
         query.setParameter(1, model);
-        return query.getResultList();
+        List<Car> cars = query.getResultList();
+        session.close();
+        return cars;
     }
 
     public List<Car> getCarByEngine(Engine engine) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT car from Car car where car.engine=: engine", Car.class);
         query.setParameter(1, engine);
-        return query.getResultList();
+        List<Car> cars = query.getResultList();
+        session.close();
+        return cars;
     }
 
     public List<Car> getCarByManufacturer(String manufacturer) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT car from Car car where car.manufacturer=: manufacturer", Car.class);
         query.setParameter(1, manufacturer);
-        return query.getResultList();
+        List<Car> cars = query.getResultList();
+        session.close();
+        return cars;
     }
 }
--- src/main/java/service/PersonService.java
+++ src/main/java/service/PersonService.java
@@ -2,30 +2,33 @@ package service;
 
 import model.Car;
 import model.Person;
-import model.Engine;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 import java.util.List;
 
 public class PersonService {
-    Session session;
-    Transaction transaction;
+    SessionFactory factory;
 
-    public PersonService(Session session) {
-        this.session = session;
-        transaction = session.beginTransaction();
+    public PersonService(SessionFactory factory) {
+        this.factory = factory;
     }
 
 
     public Person savePerson(Person Person) {
-        this.session.persist(Person);
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
+        session.persist(Person);
         transaction.commit();
+        session.close();
         return Person;
     }
 
     public Person updatePerson(Person Person) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
         Query query = session.createQuery("UPDATE Person p SET p.firstName =: first, p.lastName =: last, p.nationalId=:nationalId, p.car=: car WHERE p.id =: id", Person.class);
         query.setParameter(1, Person.getFirstName());
         query.setParameter(2, Person.getLastName());
@@ -33,52 +36,74 @@ public class PersonService {
         query.setParameter(4, Person.getCar());
         query.setParameter(5, Person.getId());
         transaction.commit();
+        session.close();
         if (query.executeUpdate() > 0)
             return Person;
         return null; //can't be updated
     }
 
     public Person deletePerson(Person person) {
+        Session session = factory.openSession();
+        Transaction transaction = session.beginTransaction();
         Query query = session.createQuery("DELETE from Person p WHERE p.id =: id");
         query.setParameter(1, person.getId());
         transaction.commit();
+        session.close();
         if (query.executeUpdate() > 0)
             return person;
         return null; //can't be deleted
     }
 
     public List<Person> selectAll() {
-        return session.createQuery("select DISTINCT person from Person person", Person.class).getResultList();
+        Session session = factory.openSession();
+        List<Person> all = session.createQuery("select DISTINCT person from Person person", Person.class).getResultList();
+        session.close();
+        return all;
     }
 
     public Person getPersonById(Long id) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT person from Person person where person.id=: id", Person.class);
         query.setParameter(1, id);
-        return (Person) query.getSingleResultOrNull();
+        Person person = (Person) query.getSingleResultOrNull();
+        session.close();
+        return person;
     }
 
     public List<Person> getPeopleByFirstName(String firstName) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT person from Person person where person.firstName=: firstName", Person.class);
         query.setParameter(1, firstName);
-        return query.getResultList();
+        List<Person> result = query.getResultList();
+        session.close();
+        return result;
     }
 
     public List<Person> getPeopleByLastName(String lastName) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT person from Person Person where person.lastName=: lastName", Person.class);
         query.setParameter(1, lastName);
-        return query.getResultList();
+        List<Person> result = query.getResultList();
+        session.close();
+        return result;
     }
 
     public Person getPersonByNationalId(String nationdalId) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT person from Person person where person.nationalId=: nationalId", Person.class);
         query.setParameter(1, nationdalId);
-        return (Person) query.getSingleResultOrNull();
+        Person perosn = (Person) query.getSingleResultOrNull();
+        session.close();
+        return perosn;
     }
 
     public List<Person> getPeopleByCar(Car car) {
+        Session session = factory.openSession();
         Query query = session.createQuery("select DISTINCT person from Person Person where person.car=: car", Person.class);
         query.setParameter(1, car);
-        return query.getResultList();
+        List<Person> result = query.getResultList();
+        session.close();
+        return result;
     }
 
 }
